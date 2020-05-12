405279f3a5c2558ba3330ca419e52a0f1f9112b7
bohatermateusz <bohatermateusz@gmail.com>
2020-05-12 17:35:13 +0200

    added notes service and dependencies

--- RPlatform/ClientApp/src/app/app.module.ts
+++ RPlatform/ClientApp/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NotesListingComponent } from './notes-listing/notes-listing.component';
 import { AddUpdateNotesComponent } from './add-update-notes/add-update-notes.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NotesService } from './notes-service.service';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,11 +23,13 @@ import { AddUpdateNotesComponent } from './add-update-notes/add-update-notes.com
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NotesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
