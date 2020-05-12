b1ff57510a8149ee3c6ffab4ef1f428840bfbd51
bohatermateusz <bohatermateusz@gmail.com>
2020-05-12 12:19:30 +0200

    app.component.ts modified to work with ASP.Net Core Web Application API

--- RPlatform/ClientApp/src/app/app.component.ts
+++ RPlatform/ClientApp/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private _httpService: Http) { }
+  accessPointUrl: string = 'https://localhost:44324/api/values';
+  apiValues: string[] = [];
   title = 'ClientApp';
+  ngOnInit() {
+    this._httpService.get(this.accessPointUrl).subscribe(values => {
+      this.apiValues = values.json() as string[];
+    })
+  }
 }
