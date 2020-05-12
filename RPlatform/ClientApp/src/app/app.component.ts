e112f29d59b0138051815fa15483b3468b13985b
bohatermateusz <bohatermateusz@gmail.com>
2020-05-13 00:13:15 +0200

    Added from begining dependencies

--- RPlatform/ClientApp/src/app/app.component.ts
+++ RPlatform/ClientApp/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private _httpService: Http) { }
+  accessPointUrl: string = 'https://localhost:44386/weatherforecast';
+  apiValues: string[] = [];
   title = 'ClientApp';
+  ngOnInit() {
+    this._httpService.get(this.accessPointUrl).subscribe(values => {
+      this.apiValues = values.json() as string[];
+    });
+  }
 }
--- RPlatform/Startup.cs
+++ RPlatform/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace RPlatform
 {
@@ -26,6 +19,14 @@ namespace RPlatform
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+                    //.AllowCredentials());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,8 @@ namespace RPlatform
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
@@ -46,6 +49,9 @@ namespace RPlatform
             {
                 endpoints.MapControllers();
             });
+
+      
+
         }
     }
 }
